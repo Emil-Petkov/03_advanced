2839a348fa4e4af37966ab5bf0279bc6b315ee20
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-06-22 23:39:45 +0300

    Update 02_stacked_queries.py

--- 01_lists_as_stacks_and_queues/02_exe/02_stacked_queries.py
+++ 01_lists_as_stacks_and_queues/02_exe/02_stacked_queries.py
@@ -4,21 +4,17 @@ numbers = []
 for _ in range(n_lines):
     operation = input().split()
 
-    if operation[0] == '2':
-        if numbers:
-            numbers.pop()
-
-    elif operation[0] == '3':
-        if numbers:
-            print(max(numbers))
-
-    elif operation[0] == '4':
-        if numbers:
-            print(min(numbers))
-
-    else:
+    if operation[0] == '1':
         add_number = int(operation[1])
         numbers.append(add_number)
+
+    elif operation[0] == '2' and numbers:
+        numbers.pop()
+
+    elif operation[0] == '3' and numbers:
+        print(max(numbers))
+    elif operation[0] == '4' and numbers:
+        print(min(numbers))
 
 result = (', '.join(map(str, reversed(numbers))))
 
