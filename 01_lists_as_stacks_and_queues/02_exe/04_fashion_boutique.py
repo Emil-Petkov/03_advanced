24e59a011ac6d984bca86cdf2ac59cd3727a918c
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-06-23 14:59:53 +0300

    Create 04_fashion_boutique.py

---
+++ 01_lists_as_stacks_and_queues/02_exe/04_fashion_boutique.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+clothes_in_the_box = deque(map(int, input().split()))
+capacity_of_one_rack = int(input())
+
+sum_of_added_clothes = 0
+
+n_racks = 1
+
+while clothes_in_the_box:
+    next_clothes = clothes_in_the_box.popleft()
+
+    if sum_of_added_clothes + next_clothes > capacity_of_one_rack:
+        n_racks += 1
+        sum_of_added_clothes = next_clothes
+
+    else:
+        sum_of_added_clothes += next_clothes
+
+print(n_racks)
