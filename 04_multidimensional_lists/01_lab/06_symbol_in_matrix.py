cea49fc8d4161de2092c7999d5f4d9158e478c93
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-07-09 20:05:08 +0300

    Create 06_symbol_in_matrix.py

---
+++ 04_multidimensional_lists/01_lab/06_symbol_in_matrix.py
@@ -0,0 +1,14 @@
+matrix = []
+
+for _ in range(int(input())):
+    matrix.append(list(input()))
+
+search_symbol = input()
+
+for row_index, row in enumerate(matrix):
+    for column_index, element in enumerate(row):
+        if search_symbol == element:
+            print(f'({row_index}, {column_index})')
+            exit()
+
+print(f'{search_symbol} does not occur in the matrix')
