96c6599acb089153ba8d6f7ab5873caaca75968b
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-07-10 22:53:14 +0300

    Create 01_diagonals.py

---
+++ 04_multidimensional_lists/02_exe/01_diagonals.py
@@ -0,0 +1,23 @@
+rows = int(input())
+
+matrix = []
+for _ in range(rows):
+    matrix.append([[int(el)] for el in input().split(', ')])
+
+primary_diagonal = []
+secondary_diagonal = []
+
+for i in range(rows):
+    primary_diagonal.append(matrix[i][i])
+    secondary_diagonal.append(matrix[i][rows - i - 1])
+
+result_primary_diagonal = [item for sublist in primary_diagonal for item in sublist]
+result_secondary_diagonal = [item for sublist in secondary_diagonal for item in sublist]
+
+print(f'Primary diagonal: {", ".join(map(str, result_primary_diagonal))}. Sum: {sum(result_primary_diagonal)}')
+print(f'Secondary diagonal: {", ".join(map(str, result_secondary_diagonal))}. Sum: {sum(result_secondary_diagonal)}')
+
+
+
+
+
