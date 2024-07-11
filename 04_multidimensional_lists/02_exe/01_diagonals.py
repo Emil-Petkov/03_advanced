def607e0058d7a2d59bb9fd1d812898e5448baaa
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-07-11 12:47:48 +0300

    Update 01_diagonals.py

--- 04_multidimensional_lists/02_exe/01_diagonals.py
+++ 04_multidimensional_lists/02_exe/01_diagonals.py
@@ -1,18 +1,9 @@
 rows = int(input())
 
-matrix = []
-for _ in range(rows):
-    matrix.append([[int(el)] for el in input().split(', ')])
+matrix = [[int(el) for el in input().split(', ')] for _ in range(rows)]
 
-primary_diagonal = []
-secondary_diagonal = []
+primary_diagonal = [matrix[r][r] for r in range(rows)]
+secondary_diagonal = [matrix[r][rows - r - 1] for r in range(rows)]
 
-for i in range(rows):
-    primary_diagonal.append(matrix[i][i])
-    secondary_diagonal.append(matrix[i][rows - i - 1])
-
-result_primary_diagonal = [item for sublist in primary_diagonal for item in sublist]
-result_secondary_diagonal = [item for sublist in secondary_diagonal for item in sublist]
-
-print(f'Primary diagonal: {", ".join(map(str, result_primary_diagonal))}. Sum: {sum(result_primary_diagonal)}')
-print(f'Secondary diagonal: {", ".join(map(str, result_secondary_diagonal))}. Sum: {sum(result_secondary_diagonal)}')
+print(f'Primary diagonal: {", ".join(map(str, primary_diagonal))}. Sum: {sum(primary_diagonal)}')
+print(f'Secondary diagonal: {", ".join(map(str, secondary_diagonal))}. Sum: {sum(secondary_diagonal)}')
