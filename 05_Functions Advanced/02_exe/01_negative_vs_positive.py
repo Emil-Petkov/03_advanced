d82e641c57fd0d72cc82c9895b2e573537803595
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-07-13 16:52:32 +0300

    Update 01_negative_vs_positive.py

--- 05_Functions Advanced/02_exe/01_negative_vs_positive.py
+++ 05_Functions Advanced/02_exe/01_negative_vs_positive.py
@@ -1,12 +1,15 @@
+def check_numbers(numbers):
+    positive_numbers_result = sum([x for x in numbers if x > 0])
+    negative_numbers_result = sum([x for x in numbers if x < 0])
+
+    print(f'{negative_numbers_result}\n{positive_numbers_result}')
+
+    if abs(positive_numbers_result) < abs(negative_numbers_result):
+        print('The negatives are stronger than the positives')
+    else:
+        print(f'The positives are stronger than the negatives')
+
+
 numbers = [int(x) for x in input().split()]
 
-positive_numbers_result = sum([x for x in numbers if x > 0])
-negative_numbers_result = sum([x for x in numbers if x < 0])
-
-print(f'{negative_numbers_result}\n{positive_numbers_result}')
-
-if abs(positive_numbers_result) < abs(negative_numbers_result):
-    print('The negatives are stronger than the positives')
-
-else:
-    print(f'The positives are stronger than the negatives')
+check_numbers(numbers)
